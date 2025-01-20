6552f6ddfc7f9ecad56f24d922978e775130cd91
Roberto Núñez <robertonc1599@gmail.com>
2025-01-19 20:00:17 -0500

    Cookies for tabbar implemented

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export * from "./sidebar/Sidebar";
 export * from "./sidebar/SidebarItem";
+export * from "./tab-bar/TabBar";
 export * from "./top-menu/TopMenu";
 export * from "./widget-item/WidgetItem";
