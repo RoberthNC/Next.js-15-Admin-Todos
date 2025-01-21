16ed86a9a9b04b866054c8295e021f18c908f9d8
Roberto Núñez <robertonc1599@gmail.com>
2025-01-20 21:03:03 -0500

    Cart Page with logic procedure have been implemented

---
+++ src/app/dashboard/cart/page.tsx
@@ -0,0 +1,47 @@
+import { Metadata } from "next";
+import { cookies } from "next/headers";
+import { Product, products } from "@/products/data/products";
+import { ItemCard } from "@/shopping-cart";
+
+export const metadata: Metadata = {
+  title: "Productos en el Carrito",
+  description: "",
+};
+
+interface ProductInCart {
+  product: Product;
+  quantity: number;
+}
+
+const getProductsInCart = (cart: { [id: string]: number }): ProductInCart[] => {
+  const productsInCart: ProductInCart[] = [];
+  for (const id of Object.keys(cart)) {
+    const product = products.find((prod) => prod.id === id);
+    if (product) {
+      productsInCart.push({ product, quantity: cart[id] });
+    }
+  }
+  return productsInCart;
+};
+
+export default async function CartPage() {
+  const cookieStore = await cookies();
+  const cart = JSON.parse(cookieStore.get("cart")?.value ?? "{}") as {
+    [id: string]: number;
+  };
+  const productsInCart = getProductsInCart(cart);
+
+  return (
+    <div>
+      <h1 className="text-5xl ">Productos en el Carrito</h1>
+      <hr className="mb-2" />
+      <div className="flex flex-col sm:flex-row gap-2 w-full">
+        <div className="flex flex-col gap-2 w-full sm:w-8/12">
+          {productsInCart.map(({ product, quantity }) => (
+            <ItemCard key={product.id} product={product} quantity={quantity} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/top-menu/TopMenu.tsx
+++ src/components/top-menu/TopMenu.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { cookies } from "next/headers";
 import {
   CiMenuBurger,
@@ -49,14 +50,17 @@ export const TopMenu = async () => {
           <button className="flex items-center justify-center w-10 h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200">
             <CiChat1 size={25} />
           </button>
-          <button className="flex p-2 items-center justify-center w-10 h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200">
+          <Link
+            href="/dashboard/cart"
+            className="flex p-2 items-center justify-center w-10 h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200"
+          >
             {totalCount > 0 && (
-              <span className="text-sm mr-2 text-blue-600 font-bold">
+              <span className="text-sm mr-2 text-blue-800 font-bold">
                 {totalCount}
               </span>
             )}
             <CiShoppingBasket size={25} />
-          </button>
+          </Link>
         </div>
       </div>
     </div>
